ea51f3149fa03d03aaa6a7535508eaab648de18e
DevSwope <141155640+DevSwope@users.noreply.github.com>
2023-08-11 12:28:24 -0400

    Add files via upload

---
+++ EnumsDemo/EnumsDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace EnumsDemo
+{
+    public enum ShippingMethod
+    {
+        RegularAirMail = 1,
+        RegisteredAirMail = 2,
+        Express = 3
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var method = ShippingMethod.Express;
+            var method2 = ShippingMethod.RegularAirMail;
+            Console.WriteLine((int)method);
+            Console.WriteLine((int)method2);
+
+            var methodId = 3;
+
+            Console.WriteLine((ShippingMethod)methodId);
+
+            Console.WriteLine(method.ToString());
+
+            Console.WriteLine(method2.ToString());
+
+            var methodName = "RegularAirMail";
+
+            var shippingMethod = (ShippingMethod)Enum.Parse(typeof(ShippingMethod), methodName);
+
+            Console.WriteLine(shippingMethod);
+        }
+    }
+}
